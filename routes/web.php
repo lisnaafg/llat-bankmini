c6c1647da28ae31b1a3ca002ea40ad0c310d737c
lisnaafg <yyylisnawati@gmail.com>
2025-03-06 12:51:57 +0700

    menambahfile

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/laporan', [LaporanController::class, 'halamanNasabah'])->middleware
 Auth::routes(["register" => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');  // Show all users
-Route::get('/users/create', [UserController::class, 'tambahUser'])->name('users.create');  // Show user creation form
-Route::post('/users', [UserController::class, 'simpanUser'])->name('users.store');  // Store new user
+Route::get('/users', [UserController::class, 'index'])->name('users.index');  
+Route::get('/users/create', [UserController::class, 'tambahUser'])->name('users.create');
+Route::post('/users', [UserController::class, 'simpanUser'])->name('users.store');
 Route::get('/users/{id}/hapus', [UserController::class, 'hapusUser'])->name('users.delete');
 Route::get('/users/{id}/edit', [UserController::class, 'editUser'])->name('users.edit');
 Route::put('/users/{id}/update', [UserController::class, 'updateUser'])->name('users.update');
@@ -25,9 +25,9 @@ Route::put('/users/{id}/update', [UserController::class, 'updateUser'])->name('u
 
 // transaksi
 Route::get('/transaksi', [TransaksiController::class, 'index'])->name('transaksi.index');
-Route::get('/transaksi/create', [TransaksiController::class, 'tambahTrans'])->name('transaksi.create');  // Show form to create transaksi
-Route::post('/transaksi/cari', [TransaksiController::class, 'cariNasabah'])->name('transaksi.cari');  // Route to search for nasabah
-Route::post('/transaksi', [TransaksiController::class, 'simpanTrans'])->name('transaksi.store');  // Store transaksi
+Route::get('/transaksi/create', [TransaksiController::class, 'tambahTrans'])->name('transaksi.create');
+Route::post('/transaksi/cari', [TransaksiController::class, 'cariNasabah'])->name('transaksi.cari');
+Route::post('/transaksi', [TransaksiController::class, 'simpanTrans'])->name('transaksi.store');
 Route::get('/transaksi/{id}/hapus', [TransaksiController::class,'hapusTrans'])->name('transaksi.delete');
 Route::get('/transaksi/{id}/edit', [TransaksiController::class,'editTrans'])->name('transaksi.edit');
 
